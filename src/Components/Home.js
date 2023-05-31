ca0b72808ddc86ff40ac34f0655139152c0c92d7
mudit247365 <guptmudit2005@gmail.com>
2023-05-31 13:04:43 +0530

    hlo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,65 +3,51 @@ import React, { useState } from 'react';
 
 function Home(props) {
   const Uppercase = () => {    
-    if (text.length>0){
+     
       console.log("Uppercase is clicked");
       let newText = text.toUpperCase();
       props.alert('Convert to Uppercase', 'Success');
       setText(newText);
     }
-    else{
-      props.alert('TextBox is empty !', 'Alert:');
-    }
     
-  };
+    
+    
+  
   const Lowercase = () => {
-    if (text.length>0){
+    
     console.log("Lowercase is clicked");
     let newtext = text.toLocaleLowerCase();
     props.alert('Convert to Lowercase', 'Success');
     setText(newtext);
-  }
-  else{
-    props.alert('TextBox is empty !', 'Alert:');
-  }  
+  } 
     
-  };
+ 
   const Clear = () => {
-    if (text.length>0){
+    
     console.log("Clear is clicked");
     let newtext = '';
     props.alert('All text is Clear', 'Success');
     setText(newtext);
-  }
-  else{
-    props.alert('TextBox is empty !', 'Alert:');
-  }
+ 
   };
   const Copy1 = () => {
-    if (text.length>0){
+    
     console.log("Copy1 is clicked");
     let newtext = text.split(/\s+/);
     props.alert('Removed extra spaces', 'Success');
     setText(newtext.join(" "));
-  }
-  else{
-    props.alert('TextBox is empty !', 'Alert:');
-  }
+ 
   };
   const Copy = () => {
     
-    if (text.length>0){
+    
     const textarea = document.getElementById('mybox');
     if (textarea) {
       textarea.select();
       textarea.setSelectionRange(0, 99999); // For mobile devices
       document.execCommand('copy');
       props.alert('Copy to Clipboard', 'Success');
-    }
-    else{
-      props.alert('TextBox is empty !', 'Alert:');
-    };
-      
+       
       
     };
   
@@ -88,17 +74,17 @@ function Home(props) {
           
           <div className="container">
             <div className="mb-3 ">
-              <textarea style={{backgroundColor: props.mode==='light'? 'grey':'white',color:props.mode==='light'? 'white':'black'}} className="form-control"id="mybox" rows={'10'}column={'50'}onChange={handleOnChange}value={text}            ></textarea>
+              <textarea style={{backgroundColor: props.mode==='light'? '#13466e':'white',color:props.mode==='light'? 'white':'black'}} className="form-control"id="mybox" rows={'10'}column={'50'}onChange={handleOnChange}value={text}            ></textarea>
             </div>
-            <button onClick={Uppercase} className="btn btn-primary  mx-2 my-2 "> Convert to Uppercase </button>
-            <button onClick={Lowercase} className="btn btn-primary   mx-2 my-2"> Convert to Lowercase </button>   
-            <button onClick={Clear} className="btn btn-primary  mx-2 my-2"> Clear </button>         
-            <button onClick={Copy}  className="btn btn-primary  mx-2 my-2"> Copy  </button>
-            <button onClick={Copy1} className="btn btn-primary  mx-2 my-2">Remove Extra Spaces</button></div>           
+            <button disabled={text.length===0} onClick={Uppercase} className="btn btn-primary  mx-2 my-2 "> Convert to Uppercase </button>
+            <button disabled={text.length===0} onClick={Lowercase} className="btn btn-primary   mx-2 my-2"> Convert to Lowercase </button>   
+            <button disabled={text.length===0} onClick={Clear} className="btn btn-primary  mx-2 my-2"> Clear </button>         
+            <button disabled={text.length===0} onClick={Copy}  className="btn btn-primary  mx-2 my-2"> Copy  </button>
+            <button disabled={text.length===0} onClick={Copy1} className="btn btn-primary  mx-2 my-2">Remove Extra Spaces</button></div>           
            
           <div className="container my-3">
             <h1 style={{color:props.mode==='light'? 'white':'black'}}>Your text summary</h1>
-            <p style={{color:props.mode==='light'? 'white':'black'}}><b>{text.split(" ").filter((element)=>{return element.length!=0}).length} words & {text.length} characters & {0.008*text.length} minutes read </b></p>
+            <p style={{color:props.mode==='light'? 'white':'black'}}><b>{text.split(" ").filter((element)=>{return element.length!=0}).length} words & {text.length} characters </b></p> {'\n'} <p style={{color:props.mode==='light'? 'white':'black'}}><b>{0.008*text.length} minutes read</b></p>  
           </div>
           <div style={{marginLeft:'12x'}}>
             <h2 style={{color:props.mode==='light'? 'white':'black'}}>Preview</h2>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,19 +4,19 @@ import React from 'react'
 function Navbar(props) {
   return (
    
-    <div><nav className={`navbar navbar-expand-lg navbar- bg-$`} style={{backgroundColor: props.mode==='light'? 'grey':'white'}}>
+    <div><nav className={`navbar navbar-expand-lg navbar- bg-$`} style={{backgroundColor: props.mode==='light'? '#13466e':'#f0ebeb'}}>
     <div className="container-fluid">
-      <a className="navbar-brand" style={{backgroundColor: props.mode==='light'? 'grey':'white'}} href="/">TextUtils</a>
+      <a className="navbar-brand" style={{color:props.mode==='light'? 'white':'black'}} href="/">TextUtils</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/Home">Home</a>
+            <a className="nav-link active" style={{color:props.mode==='light'? 'white':'black'}} aria-current="page" href="/Home">Home</a>
           </li>
           <li className={`color-${props.mode==='light'? 'dark':'light'}`}>
-            <a className="nav-link" href="/About">About</a>
+            <a className="nav-link" href="/About"></a>
           </li>
          </ul>
         {/* <form className="d-flex" role="search">
